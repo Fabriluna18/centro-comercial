e8531eba5f943f00eac6472b61f664ba09bde908
Fabriluna18 <fabrizioluna715@gmail.com>
2026-07-13 09:05:05 -0300

    feat: agrego cards de Comercio Destacados

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/sections/Hero";
 import Novedades from "../components/sections/Novedades";
 import AccesosRapidos from "../components/sections/AccesosRapidos";
+import ComerciosDestacados from "../components/sections/ComerciosDestacados";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Novedades />
       <AccesosRapidos />
+      <ComerciosDestacados />
     </main>
   );
 }
